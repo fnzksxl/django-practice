e230e68d8975b3140c683f660cc7105205bfe17f
fnzksxl <fnzksxl@gmail.com>
2024-03-14 03:09:35 +0900

    Feat: Redirect, 대문 페이지 뷰 함수 작성

--- shrinkers/shortener/views.py
+++ shrinkers/shortener/views.py
@@ -1,3 +1,22 @@
-# from django.shortcuts import render
+from django.shortcuts import render, redirect
+from shortener.models import User
 
 # Create your views here.
+
+
+def index(request):
+    user = User.objects.filter(username="admin").first()
+    email = user.email if user else "Annoymous User"
+    print(email)
+    print(request.user.is_authenticated)
+    if request.user.is_authenticated is False:
+        email = "Annoymous User"
+    print(email)
+    # render(request, 템플릿, 템플릿 내 변수)
+    return render(request, "base.html", {"welcome_msg": "Hello, World!"})
+
+
+def redirect_test(request):
+    print("Go Redirect")
+    # 이곳에서 명시하는 index는 urls.py에 있는 이름
+    return redirect("index")
