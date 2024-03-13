8480332f2b9a93696b9581eea8b25ad02c1d8faf
fnzksxl <fnzksxl@gmail.com>
2024-03-14 02:32:04 +0900

    Feat: PayPlan 모델 생성

--- shrinkers/shortener/models.py
+++ shrinkers/shortener/models.py
@@ -1,3 +1,13 @@
-# from django.db import models
+from django.db import models
 
 # Create your models here.
+# 1. 프로젝트 시작 시에 python manage.py migrate
+# 2. 커스텀 모델 작성 후 python manage.py makemigrations
+# 3. 다시 python manage.py
+
+
+class PayPlan(models.Model):
+    name = models.CharField(max_length=20)
+    price = models.IntegerField()
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
