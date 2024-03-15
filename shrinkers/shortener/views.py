e708de7cf2a8b11585e7c9237152cccbbba771ce
fnzksxl <fnzksxl@gmail.com>
2024-03-16 04:26:26 +0900

    Feat: Username 반환 API 작성

--- shrinkers/shortener/views.py
+++ shrinkers/shortener/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect
+from django.http.response import JsonResponse
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 from shortener.models import User
 
 # Create your views here.
@@ -16,7 +18,27 @@ def index(request):
     return render(request, "base.html", {"welcome_msg": "Hello, World!"})
 
 
-def redirect_test(request):
-    print("Go Redirect")
-    # 이곳에서 명시하는 index는 urls.py에 있는 이름
-    return redirect("index")
+# def redirect_test(request):
+#     print("Go Redirect")
+#     # 이곳에서 명시하는 index는 urls.py에 있는 이름
+#     return redirect("index")
+
+
+# csrf 체크를 하지 말라는 뜻
+@csrf_exempt
+def get_user(request, user_id):
+    print(user_id)
+    if request.method == "GET":
+        # Query 파라미터 얻는 법
+        abc = request.GET.get("abc")
+        xyz = request.GET.get("xyz")
+        user = User.objects.filter(pk=user_id).first()
+        return render(request, "base.html", {"user": user, "params": [abc, xyz]})
+    elif request.method == "POST":
+        username = request.GET.get("username")
+        if username:
+            user = User.objects.filter(pk=user_id).update(username=username)
+
+        return JsonResponse(
+            status=201, data=dict(msg="You just reached with Post Method!")
+        )
